3997042b4edd0b5ccbd2857ab1fc0648c54c5b12
unknown <heymiced34@gmail.com>
2026-06-04 20:24:20 -0500

    TESTS CORREGIDOS

--- novafit/config/settings.py
+++ novafit/config/settings.py
@@ -81,6 +81,12 @@ DATABASES = {
         },
     }
 }
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    }
 
 # Usuario personalizado
 AUTH_USER_MODEL = 'novafit.Usuario'
